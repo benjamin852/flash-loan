3ca6b7906f13c8b7e4d32f5c72126998ce8716f2
Ben Weinberg <weinbergben33@gmail.com>
2023-04-13 11:10:34 -0400

    Foundry setup running

--- test/FlashSwapSetup.t.sol
+++ test/FlashSwapSetup.t.sol
@@ -1,25 +1,25 @@
-// // SPDX-License-Identifier: Unlicense
-// pragma solidity ^0.7.6;
-// pragma abicoder v2;
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.7.6;
+pragma abicoder v2;
 
-// import "forge-std/Test.sol";
-// import "@uniswap/v3-periphery/contracts/interfaces/external/IWETH9.sol";
+import "forge-std/Test.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/external/IWETH9.sol";
 
-// import "../contracts/FlashSwap.sol";
+import "../contracts/FlashSwap.sol";
 
-// contract FlashSwapSetup is Test {
-//     ISwapRouter public swapRouter =
-//         ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
-//     IWETH9 public constant WETH =
-//         IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+contract FlashSwapSetup is Test {
+    ISwapRouter public swapRouter =
+        ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+    IWETH9 public constant WETH =
+        IWETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
 
-//     address public flashSwap;
+    FlashSwap public flashSwap;
 
-//     address public v3Factory;
+    address public v3Factory;
 
-//     function setUp() public {
-//         v3Factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+    function setUp() public {
+        v3Factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
 
-//         flashSwap = new FlashSwap(swapRouter, v3Factory, IWETH9);
-//     }
-// }
+        flashSwap = new FlashSwap(swapRouter, v3Factory, address(WETH));
+    }
+}
